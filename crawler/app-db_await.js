36398c17eba679f7e755a0473ce50d435da5b859
EddieWei <junnywei15@gmail.com>
2021-10-17 16:12:32 +0800

    1017 insert await版

--- crawler/app-db_await.js
+++ crawler/app-db_await.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   //port     :  "3306"   mysql預設富號通常是3306
   user: "Eddie",
-  password: "???",
+  password: "????",
   database: "stock_mfee20",
 });
 
