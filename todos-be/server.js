e61b548343bfe9f7c062b5b106fc72284d0809df
Eddiewei0617 <junnywei15@gmail.com>
2021-10-30 22:35:01 +0800

    1030 22:34 backend practice

--- todos-be/server.js
+++ todos-be/server.js
@@ -1,8 +1,17 @@
 // express 是 nodejs 的框架
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 const mysql = require("mysql");
 const Promise = require("bluebird");
+let app = express(); // application
+
+// cors
+const cors = require("cors");
+// let corsOptions = {
+//   origin: "*", // 全部
+// };
+app.use(cors());
 
 let connection = mysql.createConnection({
   host: process.env.DB_HOST, // 本機 127.0.0.1
@@ -14,14 +23,6 @@ let connection = mysql.createConnection({
 
 // 利用 bluebird 把 connection 的函式都變成 promise
 connection = Promise.promisifyAll(connection);
-
-let app = express(); // application
-
-// 準備取得todos的API
-app.get("/api/todos", async (req, res) => {
-  let data = await connection.queryAsync("SELECT * FROM todos");
-  res.json(data);
-});
 
 // app.use 告訴 express 這裡有一個中間件(middleware)
 // middleware 只是一個函式，會有三個參數
@@ -42,18 +43,10 @@ app.use((req, res, next) => {
   // 低耦合
 });
 
-// app.use(PATH, express.static(檔案夾))
-// express.static("檔案資料夾名稱")是內建的中間件
-app.use("/static", express.static("static"));
-// http://localhost:3001/static/about.html  --> 直接打那個資料夾裡面的檔名就好
-
 // 路由 router / route --> 其實也算是一種中間件
 // app.Method(Path, Handler)
 // Method: GET, POST, PUT, DELETE, PATCH,...
 // Handler 是一個函式，會有兩個參數 request, response
-app.get("/", (req, res) => {
-  res.send("我是Express首頁");
-});
 
 // 假如同時進到 /member 網址頁面，沒有next()的話，就不會進到"我是會員頁 2"這一part
 app.get("/member", (req, res, next) => {
@@ -72,6 +65,41 @@ app.get("/product", (req, res) => {
 app.get("/cart", (req, res) => {
   res.send("來到了購物車");
 });
+
+app.get("/api/test", (req, res) => {
+  res.json({
+    name: "ashley",
+    job: "engineer",
+  });
+});
+
+//-----------------------------------------------
+// 準備取得todos的API
+// 列表：全部資料
+app.get("/api/todos", async (req, res) => {
+  let data = await connection.queryAsync("SELECT * FROM todos");
+  res.json(data); // 以json格式取得資料庫的資料內容
+});
+
+// /api/todos/24 。 24是指id:24的商品
+// 根據 id 取得單筆資料
+app.get("/api/todos/:todoId", async (req, res) => {
+  // req.params.todoId --> req裡面的params功能會顯示出todoId的物件，我們再把它取出即可
+  let data = await connection.queryAsync("SELECT * FROM todos WHERE id=?;", [
+    req.params.todoId,
+  ]); // 這裡的用法是指說:要找id=多少的呢? 就是後面[]裡的數字，有點像是之前php抓資料庫打 $sql= "4,5,6"
+
+  // 以下表示: 只要有取到此商品(物件陣列)，那就顯示此商品的物件資訊；不然的話，回傳null或"NOT FOUND"
+  if (data.length > 0) {
+    res.json(data[0]); // 會回覆一個物件，而且是那"一"個商品的資料
+  } else {
+    // res.send(null)
+    res.status(404).send("NOT FOUND");
+    // 回傳錯誤的這兩種寫法都可以，端看團隊怎麼統一
+  }
+});
+
+//--------------------------------------------
 
 // 職責切割 :
 // 這個中間件是負責做紀錄
