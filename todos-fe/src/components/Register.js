af5601b4167743f83965f0ad5bd00746a111d276
Eddie Wei <junnywei15@gmail.com>
2021-10-31 17:28:04 +0800

    1031 class

--- todos-fe/src/components/Register.js
+++ todos-fe/src/components/Register.js
@@ -1,32 +1,66 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 const Register = () => {
+  const [member, setMember] = useState({
+    email: "junnywei15@gmail.com",
+    name: "Eddie",
+    password: "123123",
+    confirmPassword: "123123",
+    photo: null,
+  });
   return (
     <div className="column is-three-fifths">
       <form className="box">
         <div className="field">
           <label className="label">帳號</label>
           <div className="control">
-            <input name="email" className="input" type="email" />
+            <input
+              name="email"
+              className="input"
+              type="email"
+              value={member.email}
+              onChange={(e) => {
+                let newMember = { ...member };
+                newMember.email = e.target.value;
+                setMember(newMember);
+              }}
+            />
           </div>
         </div>
         <div className="field">
           <label className="label">密碼</label>
           <div className="control">
-            <input name="password" className="input" type="password" />
+            <input
+              name="password"
+              className="input"
+              type="password"
+              value={member.password}
+            />
           </div>
         </div>
         <div className="field">
           <label className="label">確認密碼</label>
           <div className="control">
-            <input name="confirmPassword" className="input" type="password" />
+            <input
+              name="confirmPassword"
+              className="input"
+              type="password"
+              value={member.confirmPassword}
+            />
           </div>
         </div>
         <div className="field">
           <label className="label">暱稱</label>
           <div className="control">
-            <input name="name" className="input" type="text" />
+            <input
+              name="name"
+              className="input"
+              type="text"
+              value={member.name}
+            />
           </div>
         </div>
         <div className="file is-primary is-boxed has-name mb-3">
