e36a7c138d11f201800f56dddb66d6876cd7250f
Eddiewei0617 <junnywei15@gmail.com>
2021-11-24 16:24:39 +0800

    1124

--- todos-fe/src/configs/config.js
+++ todos-fe/src/configs/config.js
@@ -1,6 +1,6 @@
 // require("dotenv").config();
 
-// export const API_URL =
-//   process.env.REACT_APP_API_URL || "http://localhost:3001/api";
+export const API_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:3001/api";
 
 // 雖然是引用.env的網址，但是預防團隊打錯變數名稱，還是設一個 "或" 直接顯示網址
